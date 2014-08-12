bdfdb8afeb161fdede3e58cd70096eb9351d03a8
Anuradha Gunasekara <theanuradha@gmail.com>
2014-08-12 09:13:20 +0530

    CORE-45: reflected changes in core

--- entirej-fx/src/org/entirej/applicationframework/fx/renderers/blocks/EJFXMultiRecordBlockRenderer.java
+++ entirej-fx/src/org/entirej/applicationframework/fx/renderers/blocks/EJFXMultiRecordBlockRenderer.java
@@ -109,7 +109,7 @@ public class EJFXMultiRecordBlockRenderer implements EJFXAppBlockRenderer
             msg = "Are you sure you want to delete the current record?";
         }
         EJMessage message = new EJMessage(msg);
-        EJQuestion question = new EJQuestion(new EJForm(_block.getForm()), "DELETE_RECORD", "Delete", message, "Yes", "No", recordToDelete);
+        EJQuestion question = new EJQuestion(new EJForm(_block.getForm()), "DELETE_RECORD", "Delete", message, "Yes", "No");
         _block.getForm().getMessenger().askQuestion(question);
         if (EJQuestionButton.ONE == (question.getAnswer()))
         {
--- entirej-fx/src/org/entirej/applicationframework/fx/renderers/blocks/EJFXTreeTableRecordBlockRenderer.java
+++ entirej-fx/src/org/entirej/applicationframework/fx/renderers/blocks/EJFXTreeTableRecordBlockRenderer.java
@@ -122,7 +122,7 @@ public class EJFXTreeTableRecordBlockRenderer implements EJFXAppBlockRenderer
             msg = "Are you sure you want to delete the current record?";
         }
         EJMessage message = new EJMessage(msg);
-        EJQuestion question = new EJQuestion(new EJForm(_block.getForm()), "DELETE_RECORD", "Delete", message, "Yes", "No", recordToDelete);
+        EJQuestion question = new EJQuestion(new EJForm(_block.getForm()), "DELETE_RECORD", "Delete", message, "Yes", "No");
         _block.getForm().getMessenger().askQuestion(question);
         if (EJQuestionButton.ONE == (question.getAnswer()))
         {
